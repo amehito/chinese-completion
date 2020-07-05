b4cc9caf19ac2d355bd80bfe15d0c5db40195726
zuozijian <zuozijian@bytedance.com>
2020-07-05 15:57:42 +0800

    fix: 之前对 api 理解有问题，重新写了一下

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # chinese-completion
 中文补全插件
 - [x] 单文字中文匹配
-- [x] 保留原补全项的所有信息，如类型，path 等
-- [ ] 保留原补全项的 insertHandle
 - [ ] 高亮匹配的中文
 - [ ] 多音字支持
--- src/main/kotlin/com/zzj/chineseCompletion/ChineseCompletionContributor.kt
+++ src/main/kotlin/com/zzj/chineseCompletion/ChineseCompletionContributor.kt
@@ -5,63 +5,33 @@ import com.intellij.codeInsight.completion.CompletionContributor
 import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionResultSet
 import com.intellij.codeInsight.completion.impl.CamelHumpMatcher
-import com.intellij.codeInsight.lookup.LookupElement
-import com.intellij.codeInsight.lookup.LookupElementBuilder
-import com.intellij.codeInsight.lookup.LookupElementPresentation
-import com.intellij.codeInsight.lookup.LookupElementRenderer
 
 class ChineseCompletionContributor : CompletionContributor() {
-
     override fun fillCompletionVariants(parameters: CompletionParameters, result: CompletionResultSet) {
         super.fillCompletionVariants(parameters, result)
-        val myMatcher = ChineseAndCamelHumpMatcher(result.prefixMatcher.prefix, false)
-        result.withPrefixMatcher(myMatcher)
-            .runRemainingContributors(parameters) { el ->
-                val ele = el.lookupElement
-                val newName = myMatcher.nameMap[ele.lookupString]
-                if (newName != null) {
-                    result.addElement(
-                        LookupElementBuilder.create(newName)
-                            .withRenderer(object : LookupElementRenderer<LookupElement>() {
-                                override fun renderElement(
-                                    element: LookupElement?,
-                                    presentation: LookupElementPresentation?
-                                ) {
-                                    ele.renderElement(presentation)
-                                }
-                            })
-                            .withInsertHandler { ctx, _ ->
-                                ctx.document.replaceString(
-                                    ctx.startOffset,
-                                    ctx.tailOffset,
-                                    ele.lookupString
-                                )
-                            }
-                    )
-                } else {
-                    result.addElement(ele)
-                }
-            }
+        result.withPrefixMatcher(ChineseAndCamelHumpMatcher(result.prefixMatcher.prefix, false)).also { newResult ->
+            newResult.runRemainingContributors(parameters) { newResult.addElement(it.lookupElement) }
+        }
     }
 }
 
 class ChineseAndCamelHumpMatcher(prefix: String, caseSensitive: Boolean) : CamelHumpMatcher(prefix, caseSensitive) {
-    val nameMap = mutableMapOf<String, String>()
+    private val matchResultCache = mutableMapOf<String, Boolean>()
+    private val isChineseCache = mutableMapOf<String, Boolean>()
+
+    private fun isChinese(name: String) = isChineseCache.getOrPut(name, { name.any { Pinyin.isChinese(it) } })
+
+    private fun toPinyin(name: String) = name.toCharArray().joinToString("") {
+        if (Pinyin.isChinese(it))
+            Pinyin.toPinyin(it).run { first() + drop(1).toLowerCase() }
+        else
+            it.toString()
+    }
+
     override fun prefixMatches(name: String): Boolean {
         val matchOld = super.prefixMatches(name)
-        if (!matchOld && name.any { Pinyin.isChinese(it) }) {
-            val newName = name
-                .toCharArray()
-                .joinToString("") {
-                    if (Pinyin.isChinese(it))
-                        "$it(${Pinyin.toPinyin(it).toLowerCase()})"
-                    else
-                        "$it"
-                }
-            if (super.prefixMatches(newName)) {
-                nameMap[name] = newName
-                return true
-            }
+        if (!matchOld && isChinese(name)) {
+            return matchResultCache.getOrPut(name, { super.prefixMatches(toPinyin(name)) })
         }
         return matchOld
     }
