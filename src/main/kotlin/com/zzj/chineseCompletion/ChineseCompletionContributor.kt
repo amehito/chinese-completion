047d38c3a6f5ab4dfa41133f4373cc2cb0700d8c
zuozijian <zuozijian@bytedance.com>
2020-07-05 20:56:52 +0800

    fix: 补全面板关闭的原因找到了，因为没有重写 cloneWithPrefix 函数，导致每次 prefix 改变调用 clone 的时候都会变回 CamelHumpMatcher

--- src/main/kotlin/com/zzj/chineseCompletion/ChineseCompletionContributor.kt
+++ src/main/kotlin/com/zzj/chineseCompletion/ChineseCompletionContributor.kt
@@ -1,22 +1,23 @@
 package com.zzj.chineseCompletion
 
 import com.github.promeg.pinyinhelper.Pinyin
-import com.intellij.codeInsight.completion.CompletionContributor
-import com.intellij.codeInsight.completion.CompletionParameters
-import com.intellij.codeInsight.completion.CompletionResultSet
+import com.intellij.codeInsight.completion.*
 import com.intellij.codeInsight.completion.impl.CamelHumpMatcher
+import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.codeInsight.lookup.LookupElementPresentation
+import com.intellij.codeInsight.lookup.LookupElementRenderer
 
 class ChineseCompletionContributor : CompletionContributor() {
     override fun fillCompletionVariants(parameters: CompletionParameters, result: CompletionResultSet) {
-        result.restartCompletionOnAnyPrefixChange()
         result.withPrefixMatcher(ChineseAndCamelHumpMatcher(result.prefixMatcher.prefix, false)).also { newResult ->
             newResult.runRemainingContributors(parameters) { newResult.passResult(it) }
         }
     }
 }
 
-class ChineseAndCamelHumpMatcher(prefix: String, caseSensitive: Boolean) : CamelHumpMatcher(prefix, caseSensitive) {
-    private fun isChinese(name: String) = name.any { Pinyin.isChinese(it) }
+class ChineseAndCamelHumpMatcher(prefix: String, private val caseSensitive: Boolean) :
+    CamelHumpMatcher(prefix, caseSensitive) {
 
     private fun toPinyin(name: String) = name.toCharArray().joinToString("") {
         if (Pinyin.isChinese(it)) Pinyin.toPinyin(it).toLowerCase().capitalize() else it.toString()
@@ -24,9 +25,13 @@ class ChineseAndCamelHumpMatcher(prefix: String, caseSensitive: Boolean) : Camel
 
     override fun prefixMatches(name: String): Boolean {
         val matchOld = super.prefixMatches(name)
-        if (!matchOld && isChinese(name)) {
+        if (!matchOld && name.any { Pinyin.isChinese(it) }) {
             return super.prefixMatches(toPinyin(name))
         }
         return matchOld
+    }
+
+    override fun cloneWithPrefix(s: String): PrefixMatcher {
+        return takeIf { s == prefix } ?: ChineseAndCamelHumpMatcher(s, caseSensitive)
     }
 }
